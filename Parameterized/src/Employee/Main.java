7b5363a5e7b8bfb832f39669ca85fbe4e912a5b8
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:59:52 +0530

    commit my code

---
+++ Parameterized/src/Employee/Main.java
@@ -0,0 +1,23 @@
+package Employee;
+
+public class Main {
+
+		public static void main(String[] args) {
+
+		Employee e1 = new Employee();
+
+		Employee e2 = new Employee();
+
+		Employee e3 = new Employee();
+
+		e1.display();
+
+		e2.display();
+
+		e3.display();
+
+		}
+
+		}
+
+
