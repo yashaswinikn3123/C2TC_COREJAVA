ec5750421223f30436a993c0217e72840169ce7c
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:35:53 +0530

    commit my code

---
+++ Constructor/src/arg/Employee.java
@@ -0,0 +1,26 @@
+package arg;
+
+public class Employee {
+
+		int id;
+
+		String name;
+
+		float salary;
+
+		Employee() {
+
+		System.out.println("user defined no-argument "
+
+		+ "constructor executed");
+
+		}
+
+		void display() {
+
+		System.out.println(id + " " + name + " " + salary);
+
+		}
+
+		}
+
