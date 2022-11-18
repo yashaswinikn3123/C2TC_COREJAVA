2b6fb44b6c69d153664d3648b603b33886559ba1
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:37:47 +0530

    commit my code

---
+++ Cricket/src/Test/Demo.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class Demo {
+	int x;
+
+	}
+
+	class TestDemo{
+
+	public static void main(String[]
+	args) {
+
+	Demo myObj = new Demo();
+
+	myObj.x = 40;
+
+	System.out.println(myObj.x);
+
+	}
+
+}
