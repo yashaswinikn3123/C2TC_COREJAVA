5f0b36c8ac8119cd0fe0e53cd6142dced6c0ea3d
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:58:44 +0530

    commit my code

---
+++ Numbers/src/numer/Numerics.java
@@ -0,0 +1,20 @@
+package numer;
+
+public class Numerics {
+	public static void main(String[] args) {
+
+		int x = 10;
+
+		int y = 12;
+
+		if(x+y < 10) {
+
+		System.out.println("x + y is less than 10");
+
+		}else {
+
+		System.out.println("x + y is greater than 20"); }
+
+		}
+
+}
