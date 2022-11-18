7e68f5135d0f661a5c01d2d206a88fc918b6affd
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:56:00 +0530

    commit my code

---
+++ Languages/src/language/Languages.java
@@ -0,0 +1,26 @@
+package language;
+
+public class Languages {
+	public static void main(String[] args) {
+
+		String lang = "Java";
+
+		if(lang == "Python") {
+
+		System.out.println("language is python");
+
+		}else if (lang == "C++") {
+
+		System.out.println("lang is C++");
+
+		}else if(lang == "Java") {
+
+		System.out.println("lang is Java");
+
+		}else {
+
+		System.out.println(lang); }
+
+		}}
+
+
