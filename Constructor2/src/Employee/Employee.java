2f16333bda8a12cbb476fcba8b8b36fddb8d153f
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:36:41 +0530

    commit my code

---
+++ Constructor2/src/Employee/Employee.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class Employee {
+
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Constructor2/src/clas/Main.java
@@ -0,0 +1,21 @@
+package clas;
+
+import Employee.Employee;
+
+public class Main {
+
+		public static void main(String[] args) {
+
+		Employee e1 = new Employee();
+
+		Employee e2 = new Employee();
+
+		e1.display();
+
+		e2.display();
+
+		}
+
+		}
+
+
