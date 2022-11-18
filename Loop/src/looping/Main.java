987f4e6ca512d5976583acc703ae62195a0fea9f
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-18 21:56:34 +0530

    commit my code

---
+++ Loop/src/looping/Main.java
@@ -0,0 +1,21 @@
+package looping;
+
+public class Main {
+
+public static void main(String[] args) {
+
+for(int i = 1;i <= 3;i++) {
+
+for(int j = 1;j <= 3;j++) {
+
+System.out.println(i+" "+j);
+
+}
+
+}
+
+}
+
+}
+
+
