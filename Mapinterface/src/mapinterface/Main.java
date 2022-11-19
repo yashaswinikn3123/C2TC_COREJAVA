e98c0fe46da456eda367af10a6c103852c51b93c
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 17:39:24 +0530

    commit my code

---
+++ Mapinterface/src/mapinterface/Main.java
@@ -0,0 +1,31 @@
+package mapinterface;
+
+	import java.util.Map;
+
+	import java.util.Map;
+
+	import java.util.HashMap;
+
+	@SuppressWarnings("unused")
+	public class Main
+
+	{
+
+	public static void main(String args[])
+
+	{
+
+	Map<Integer,String> map=new HashMap<Integer,String>();
+
+	map.put(95,"Welcome");
+
+	map.put(100,"to");
+
+	map.put(80,"webinar");
+
+	System.out.println(map);
+
+	}
+
+	}
+
