4a503d6b820f4537d35ea4b9f3072330b08caf56
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 21:13:49 +0530

    commit my code

---
+++ Keymodifier/src/keymodify/Student.java
@@ -0,0 +1,15 @@
+package keymodify;
+
+class Student{  
+	   int rollno; 
+	   String name;  
+	   static String college ="ITS";
+	 
+	   Student(int r, String n){  
+	   rollno = r;  
+	   name = n;  
+	   }  
+}
+	  
+	   
+
