890cbf62304850f807bb0416affa65bcfa9a046e
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 16:33:31 +0530

    commit my code

---
+++ Collection/src/collection/Main.java
@@ -0,0 +1,33 @@
+package collection;
+
+public class Main
+
+{
+
+public static void main(String[] args)
+
+{
+
+int arr[]=new int[8];
+
+arr[0]=10;
+
+arr[1]=20;
+
+arr[2]=60;
+
+arr[3]=80;
+
+arr[4]=100;
+
+arr[5]=(int) 9.2f;
+
+for(int i=0;i<arr.length;i++)
+
+System.out.println(arr[i]);
+
+}
+
+}
+
+
