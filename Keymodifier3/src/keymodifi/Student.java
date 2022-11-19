8586120155ae5a937346e7a5bd41d78fc5bd0ab3
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 21:47:31 +0530

    commit my code

---
+++ Keymodifier3/src/keymodifi/Student.java
@@ -0,0 +1,35 @@
+package keymodifi;
+
+class Student{  
+    int rollno;  
+    String name;  
+    static String college = "ITS";  
+    
+    static void change(){  
+    college = "BBDIT";  
+    }  
+     
+    Student(int r, String n){  
+    rollno = r;  
+    name = n;  
+    }  
+   
+    void display(){System.out.println(rollno+" "+name+" "+college);  }
+   
+ 
+
+  
+   public static void main(String args[]){  
+   Student.change();  
+   
+   Student s1 = new Student(111,"Karan");  
+   Student s2 = new Student(222,"Aryan");  
+   Student s3 = new Student(333,"Sonoo");  
+  
+   s1.display();  
+   s2.display();  
+   s3.display();  
+   }  
+}  
+
+
