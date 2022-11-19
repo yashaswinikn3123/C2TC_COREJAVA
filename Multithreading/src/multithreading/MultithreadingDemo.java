21aa351eb2e501f4e02a27e07c86d24baa3cd2d4
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 15:27:14 +0530

    commit my code

---
+++ Multithreading/src/multithreading/MultithreadingDemo.java
@@ -0,0 +1,17 @@
+package multithreading;
+
+class MultithreadingDemo implements Runnable {
+    public void run()
+    {
+        try {
+       
+            System.out.println(
+                "Thread " + Thread.currentThread().getId()
+                + " is running");
+        }
+        catch (Exception e) {
+          
+            System.out.println("Exception is caught");
+        }
+    }
+}
