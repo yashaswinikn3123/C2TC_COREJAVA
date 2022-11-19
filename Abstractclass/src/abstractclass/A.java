4b377cf8b01a2b19d877e43a584f0e8ab1c10192
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 15:02:05 +0530

    commit my code

--- Abstractclass/src/abstractclass/A.java
+++ Abstractclass/src/abstractclass/A.java
@@ -1,5 +1,39 @@
 package abstractclass;
 
-public class A {
+abstract class Animal
+
+{
+
+abstract void eat();
 
 }
+
+class Herbivores extends Animal
+
+{
+
+void eat()
+
+{
+
+System.out.println("I am a vegetarian");
+
+}
+
+}
+
+class Carnivores extends Animal
+
+{
+
+void eat()
+
+{
+
+System.out.println("I am non-vegetarian");
+
+}
+
+}
+
+
