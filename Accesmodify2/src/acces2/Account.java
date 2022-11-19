f0ee1a4828c4d5d71593f9b9813be97ce892bd4f
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 20:59:31 +0530

    commit my code

---
+++ Accesmodify2/src/acces2/Account.java
@@ -0,0 +1,45 @@
+package acces2;
+
+public class Account {
+
+	public void setAcc_no(long l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setName(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getEmail() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setAmount(float f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEmail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getAcc_no() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
