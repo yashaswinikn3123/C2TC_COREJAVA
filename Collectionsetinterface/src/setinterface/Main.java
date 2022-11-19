a1020ab8157676330757fefc13d255badec7bb5e
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 17:19:52 +0530

    commit my code

---
+++ Collectionsetinterface/src/setinterface/Main.java
@@ -0,0 +1,32 @@
+package setinterface;
+
+import java.util.Set;
+
+import java.util.HashSet;
+
+public class Main {
+
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public static void main(String args[])
+
+{
+
+Set obj= new HashSet();
+
+obj.add("Technology");
+
+obj.add("Practice");
+
+obj.add("Java");
+
+obj.add(4.56);
+
+obj.add(1);
+
+System.out.println(obj);
+
+}
+
+}
+
+
