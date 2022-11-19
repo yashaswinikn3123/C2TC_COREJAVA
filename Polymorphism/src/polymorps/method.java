a97a301ab9aba6ea0864c3f75df42fd6bdb31217
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 15:13:59 +0530

    commit my code

---
+++ Polymorphism/src/polymorps/method.java
@@ -0,0 +1,25 @@
+package polymorps;
+
+class Bank{
+
+int getRateOfInterest(){return 0;}
+
+}
+
+class SBI extends Bank{
+
+int getRateOfInterest(){return 8;}
+
+}
+
+class ICICI extends Bank{
+
+int getRateOfInterest(){return 7;}
+
+}
+
+class AXIS extends Bank{
+
+int getRateOfInterest(){return 9;}
+
+}
