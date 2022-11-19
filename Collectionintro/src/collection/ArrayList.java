d08f6be03ff7da63551eddf96ff81ce56a7403e1
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 16:35:05 +0530

    commit my code

---
+++ Collectionintro/src/collection/ArrayList.java
@@ -0,0 +1,26 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class arrayList
+
+{
+public static void main(String [] args)
+
+{
+Collection list = new ArrayList();
+
+list.add(100);
+list.add(10);
+list.add(1000);
+list.add(list“Webinar”);
+
+System.out.println("ArrayList : " + list);
+
+}
+
+}
+
+
---
+++ Collectionintro/src/collectionintro/arrayList.java
@@ -0,0 +1,29 @@
+package collectionintro;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+class arrayList
+
+{
+@SuppressWarnings("unchecked")
+public static void main(String [] args)
+
+{
+@SuppressWarnings("rawtypes")
+Collection list = new ArrayList();
+
+list.add(100);
+list.add(10);
+list.add(1000);
+list.add(list“Webinar”);
+
+System.out.println("ArrayList : " + list);
+
+}
+
+}
+
+
+
