4ec7dfb18378247f1f44a9b7e064124ba4a2b088
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 15:09:02 +0530

    commit my code

--- Methodoverloading/src/overloading/Vehicle.java
+++ Methodoverloading/src/overloading/Vehicle.java
@@ -1,5 +1,33 @@
 package overloading;
 
-public class Vehicle {
+class Vehicle{
+
+void run()
+
+{
+
+System.out.println("Vehicle is running");
+
+}
+
+}
+
+class Bike2 extends Vehicle{
+
+void run()
+
+{
+
+System.out.println("Bike is running safely");
+
+}
+
+public static void main(String args[]){
+
+Bike2 obj = new Bike2();
+
+obj.run();
+
+}
 
 }
