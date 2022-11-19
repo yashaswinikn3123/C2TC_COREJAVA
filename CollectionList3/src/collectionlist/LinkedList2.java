087a56ea736eee65b5a26d7353ca17cd3e210097
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-11-19 17:02:03 +0530

    commit my code

---
+++ CollectionList3/src/collectionlist/LinkedList2.java
@@ -0,0 +1,23 @@
+package collectionlist;
+
+import java.util.*;
+
+public class LinkedList2{
+
+public static void main(String args[]){
+
+LinkedList<String> ll=new LinkedList<String>();
+
+ll.add("Ravi");
+
+ll.add("Vijay");
+
+ll.add("Ajay");
+
+System.out.println("After invoking add(E e) method: "+ll);
+
+}
+
+}
+
+
