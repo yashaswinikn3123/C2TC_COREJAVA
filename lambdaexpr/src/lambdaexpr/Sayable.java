c922310a31265e27fa686b9ae202fe46a8f680a3
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-12-14 18:29:44 +0530

    commit the code

---
+++ lambdaexpr/src/lambdaexpr/Sayable.java
@@ -0,0 +1,17 @@
+package lambdaexpr;
+
+class Sayable {
+
+		public void say(){
+		System.out.println("I have nothing to say");
+		}
+		public class Main{
+		public static void main(String[] args)
+		{
+		Sayable s= new Sayable();
+		s.say();
+		}
+		}
+}
+		
+		
