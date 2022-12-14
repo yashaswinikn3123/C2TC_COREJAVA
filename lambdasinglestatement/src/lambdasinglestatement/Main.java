af7191092c584ff687740b47a7d5e5d5c974dba7
Yashaswini K M <yashaswinikn3123@gmail.com>
2022-12-14 18:31:20 +0530

    commit the code

---
+++ lambdasinglestatement/src/lambdasinglestatement/Main.java
@@ -0,0 +1,14 @@
+package lambdasinglestatement;
+
+	interface Sayable {
+		public void say();
+		}
+		public class Main{
+		public static void main(String[] args)
+		{
+		Sayable s=() -> { System.out.println("I have nothing to say"); } ;
+		s.say();
+		}
+		}
+
+
