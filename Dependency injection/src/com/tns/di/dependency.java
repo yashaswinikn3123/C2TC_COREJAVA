8c6a7ff9dc45400fa115d3d8ba71ddc83fef710a
Yashaswini K M <yashaswinikn3123@gmail.com>
2023-02-19 14:15:26 +0530

    executed

---
+++ Dependency injection/src/com/tns/di/dependency.java
@@ -0,0 +1,22 @@
+package com.tns.di;
+{
+public class dependency {
+	  
+	  public String StudentName;
+	  public int id;
+
+	 public void setId(int id)
+	{
+	 this.id = id;
+	}
+	 public void setStudentName(String studentName)
+	{
+	 this.StudentName =studentName;
+	}
+	 
+		 public void disp() 
+	}
+       System.out.println(&quot;student name is &quot;+&quot; &quot;+StudentName+&quot;id is&quot;+id);
+       }
+
+
